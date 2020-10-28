bb347f644b3287965b2c214c401d4fe1c97bd77d
PabloCorrea99 <pcorream2@eafit.edu.co>
2020-10-28 08:40:01 -0500

    cambios controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,7 @@ class Controller extends BaseController
         "Anyone who has never made a mistake has never tried anything new - Albert Einstein",
         "Never Stop Exploring - The North Face",
         "Be yourself; everyone else is already taken - Oscar Wilde",
-        "So many books, so little time - Frank Zappa",
+        "So many books, so little time! - Frank Zappa",
         "Be the change that you wish to see in the world - Mahatma Gandhi",
     );
 
@@ -19,7 +19,7 @@ class Controller extends BaseController
         $totalQuotes = (count(Controller::$quotes));
         $randomNumber = (rand(0,($totalQuotes-1)));
         $randomQuote = Controller::$quotes[$randomNumber];
-        return response()->json(['quote' => $randomQuote]);
+        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
     }
 
 }
